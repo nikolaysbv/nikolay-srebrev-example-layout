9e49a2370d040b9c43ab69700c47f818d3035d8a
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-07-20 18:12:15 +0300

    created build and set up scripts

--- server.js
+++ server.js
@@ -7,20 +7,17 @@ import path from "path";
 
 import data from "./data/mock-data.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.get("/api/v1/cards", (req, res) => {
   res.status(200).json(data);
 });
 
-app.get("*", (req, res) => {
-  res.send("Hi!");
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
-
-// uncomment after build
-// const dir = dirname(fileURLToPath(import.meta.url));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(dir, "./client/build", "index.html"));
-// });
 
 app.listen(5000, () => {
   console.log(`Server is listening on port 5000...`);
